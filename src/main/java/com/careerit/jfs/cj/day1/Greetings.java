07b19d1a9e48c68e914a5564cc8c7dd010fcb003
javawithlwl <javawithlwl@gmail.com>
2023-04-12 08:35:08 +0530

    Added extra log

--- src/main/java/com/careerit/jfs/cj/day1/Greetings.java
+++ src/main/java/com/careerit/jfs/cj/day1/Greetings.java
@@ -7,5 +7,6 @@ public class Greetings {
       username = args[0];
     }
     System.out.println("Hi "+username+",\n\t\"Welcome to Java World\"");
+    System.out.println("Hi "+username+",\n\t\"Welcome to Java World\"");
   }
 }
