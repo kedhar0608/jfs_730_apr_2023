c0de82057be79aa7cba733ab01fc16a8571a9e93
javawithlwl <119342072+javawithlwl@users.noreply.github.com>
2023-04-13 07:50:42 +0530

    Dev (#2)
    * Added extra log message and multiplication of table added
    
    * Add quotes for message
    
    * Added extra log
    
    * Removed Extra log message
    
    * Factorial of given number logic added
    
    * Factorial of given number logic added
    
    * Refactored logic
    
    * Factorial of given number logic added
    
    * updated default username

---
+++ src/main/java/com/careerit/jfs/cj/day1/FactorialOfNumber.java
@@ -0,0 +1,12 @@
+package com.careerit.jfs.cj.day1;
+
+public class FactorialOfNumber {
+  public static void main(String[] args) {
+    int num = 5;
+    int res = 1;
+    for (int i = 2; i <= num; i++) {
+      res *= i;
+    }
+    System.out.println(String.format("Factorial of %s is %s",num,res));
+  }
+}
--- src/main/java/com/careerit/jfs/cj/day1/Greetings.java
+++ src/main/java/com/careerit/jfs/cj/day1/Greetings.java
@@ -2,10 +2,10 @@ package com.careerit.jfs.cj.day1;
 
 public class Greetings {
   public static void main(String[] args) {
-    String username = "User";
+    String username = "Username";
     if (args.length > 0) {
       username = args[0];
     }
-    System.out.println("Hi "+username+",\n\tWelcome to Java World");
+    System.out.println("Hi "+username+",\n\t\"Welcome to Java World\"");
   }
 }
