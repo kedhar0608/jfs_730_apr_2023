d9be8bd3f6b8911077c9bb5c67ba595b53e63422
kedhar0608 <vermakedhar@gmail.com>
2023-04-14 20:24:41 -0500

    Removed int Sum

--- src/main/java/com/careerit/jfs/cj/day1/SumOfTwoNumbers.java
+++ src/main/java/com/careerit/jfs/cj/day1/SumOfTwoNumbers.java
@@ -5,7 +5,6 @@ public class SumOfTwoNumbers {
   public static void main(String[] args) {
     int num1 = 10;
     int num2 = 20;
-    int sum = num1 + num2;
     System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
   }
 }
